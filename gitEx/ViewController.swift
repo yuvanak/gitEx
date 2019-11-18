c10be5addba0a85fbf9da7418304a51a251e1c95
yuvanak <devyuvana@gmail.com>
2019-11-18 01:01:19 -0800

    Add view did disappear

--- gitEx/ViewController.swift
+++ gitEx/ViewController.swift
@@ -19,5 +19,8 @@ class ViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         
     }
+    override func viewDidDisappear(_ animated: Bool) {
+        <#code#>
+    }
 }
 
